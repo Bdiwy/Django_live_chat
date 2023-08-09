c1c3d19da391bdbae891e591796d2a8f611293d6
Ahmed Bdiwy <ahmedbedawi22@gmail.com>
2023-08-09 14:27:03 +0300

    updates

--- chat/views.py
+++ chat/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-          pass
+        return render(request,'index.html')
+
+
+def room(request):
+        return render(request,'room.html')
